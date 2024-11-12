d686df6ca913602fc921e8ab7486b718ce1123b2
Bhanu Pratap Singh <bhanu.singh@agmatix.com>
2024-11-12 14:42:28 +0530

    Created mardown editor component

---
+++ src/components/Editor.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Editor.css'
+
+const Editor = () => {
+    const [markdown, setMarkdown] = useState('');
+    const [html, setHtml] = useState('');
+
+    const handleInputChange = (e) => {
+        setMarkdown(e.target.value);
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await axios.post('http://localhost:9000/convert', { markdown });
+                setHtml(result.data.html);
+            } catch (err) {
+                console.error(err.message);
+            }
+        };
+        fetchData();
+    }, [markdown]);
+
+    return (
+        <div className="editor">
+            <textarea
+                value={markdown}
+                onChange={handleInputChange}
+                placeholder="Type your Markdown here..."
+            />
+            <div
+                className="preview"
+                dangerouslySetInnerHTML={{ __html: html }} // Updated to render HTML
+            />
+        </div>
+    );
+};
+
+export default Editor;
